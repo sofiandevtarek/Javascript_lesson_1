20a19104407306fc59decac4609d8a11c40855a8
AL Tarek Sofian dev <sofiandev815@gmail.com>
2025-06-22 06:13:46 +0300

    u

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function changeColor() {
-  const colors = ["red", "green", "blue", "yellow", "pink", "grey", "black"];
+  const colors = ["red", "green", "blue", "yellow", "pink", "grey", "black", "purple", "orange"];
   const random = Math.floor(Math.random() * colors.length);
   const selsctedColor = colors[random];
   document.querySelector("body").style.background = selsctedColor;
