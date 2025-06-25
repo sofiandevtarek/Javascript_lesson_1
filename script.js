6143e6aaf7dfe8a35a56c53a03fa08ba627b9d11
AL Tarek Sofian dev <sofiandev815@gmail.com>
2025-06-25 12:53:12 +0300

    some updates to be better

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function changeColor() {
-  const colors = ["red", "green", "blue", "yellow", "pink", "grey", "black", "purple", "orange"];
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function () {
+  const colors = [
+    "red",
+    "green",
+    "blue",
+    "yellow",
+    "pink",
+    "grey",
+    "black",
+    "purple",
+    "orange",
+  ];
   const random = Math.floor(Math.random() * colors.length);
   const selsctedColor = colors[random];
   document.querySelector("body").style.background = selsctedColor;
-}
+});
