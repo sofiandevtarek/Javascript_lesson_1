159d29a0516be86871e7cb550fddf639cf8b1a45
AL Tarek Sofian dev <sofiandev815@gmail.com>
2025-07-03 09:18:00 +0300

    u

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-const btn = document.querySelector(".btn");
+const colorChanger = document.getElementById("change-color");
+const counterValue = document.getElementById("counter-value");
+const increment = document.getElementById("increment");
+const decrement = document.getElementById("decrement");
+const reset = document.getElementById("reset");
+let counter = 0;
 
-btn.addEventListener("click", function () {
-  const colors = [
-    "red",
-    "green",
-    "blue",
-    "yellow",
-    "pink",
-    "grey",
-    "black",
-    "purple",
-    "orange",
-  ];
+counterValue.innerText = counter;
+
+// increment
+increment.addEventListener("click", () => {
+  counter += 2;
+  counterValue.innerText = counter;
+});
+
+// decrement
+decrement.addEventListener("click", () => {
+  counter--;
+  counterValue.innerText = counter;
+});
+
+// reset
+reset.addEventListener("click", () => {
+  counter = 0;
+  counterValue.innerText = counter;
+});
+
+counterValue.addEventListener("click", () => { 
+  const colors = ["red", "green", "yellow"];
   const random = Math.floor(Math.random() * colors.length);
-  const selsctedColor = colors[random];
-  document.querySelector("body").style.background = selsctedColor;
+  const selectedColor = colors[random];
 });
